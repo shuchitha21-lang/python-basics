96ad2eacdfc55f023d3b78d46cd12e7938e348dc
Shuchitha S <shuchithasrinivas21@gmail.com>
2026-05-22 16:50:05 +0000

    Added Day2 Python Practice

--- day02.py
+++ day02.py
@@ -1,9 +1,31 @@
 name="Shuchitha"
 age=21
-course="BCA"
+is_student=True
+height=5.1
+fruits=["mango, jackfruit, watermelon"]
+animals=("dog", "cat", "cheetha")
 print("My name is",name)
 print("I am",age,"years old")
-print(course)
-
+#input function
 city=input("Enter your city:")
 print("You live in", city)
+#datatypes
+print(type(name))
+print(type(age))
+print(type(is_student))
+print(type(height))
+print(type(fruits))
+print(type(animals))
+#operators
+num1=int(input("Enter number1:"))
+num2=int(input("Enter number2:"))
+print("Addition:",num1+num2)
+print("Subtraction:",num1-num2)
+print("Multiplication:",num1*num2)
+print("Division:",num1/num2)
+print("Module:",num1%num2)
+num=int(input("Enter any number"))
+print(num+100)
+print(num-30)
+print(num*11)
+print(num/5)
