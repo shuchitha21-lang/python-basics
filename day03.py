456b0d070f8c593f0fb7bf8c1a634ec0a5e38e4c
Shuchitha S <shuchithasrinivas21@gmail.com>
2026-05-23 18:35:16 +0000

    Added Day3 Python Practice

--- day03.py
+++ day03.py
@@ -2,4 +2,15 @@ age=int(input("Enter your age:"))
 if age>=18:
     print("You are eligibile for voting")
 else:
-    print("You are not eligibile for voting")    
+    print("You are not eligibile for voting")   
+
+marks=int(input("Enter your marks"))
+if marks>=90:
+    print("Excellence")
+if marks>=75:
+    print("Good Work")
+if marks>=50:
+    print("Better luck next time")
+else:
+    print("Fail")
+    
