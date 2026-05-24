f39258dad412eb60dc1902e01a73d4d5ecec2a5b
Shuchitha S <shuchithasrinivas21@gmail.com>
2026-05-24 18:31:58 +0000

    Added Day4 Python Practice

---
+++ day04.py
@@ -0,0 +1,9 @@
+marks=int(input("Enter your marks:"))
+if marks>=90:
+    print("Grade A")
+elif marks>=75:
+    print("Grade B")
+elif marks>=50:
+    print("Grade C")
+else:
+    print("Grade D")       
