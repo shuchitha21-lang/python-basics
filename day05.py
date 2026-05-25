cf106a806bf3a5e37858177d4ebd2a59a5d6b598
Shuchitha S <shuchithasrinivas21@gmail.com>
2026-05-25 18:25:58 +0000

    Added Day5 Python Practice

---
+++ day05.py
@@ -0,0 +1,15 @@
+# Loop Statements
+for i in range(0,8):
+    print(i)
+# loop statement to print words    
+for i in range(1,11):
+    print("Hello")  
+#to print only even numbers    
+for i in range(2,11,2):
+    print(i)
+#to print odd numbers      
+for i in range(1,12,2):
+    print(i)
+#To print reverse 10 to 1
+for i in range(10,0,-1):
+    print(i)    
