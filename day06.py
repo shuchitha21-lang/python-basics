cdf08155dc6a54ed63f0c07b7221280edb784d77
Shuchitha S <shuchithasrinivas21@gmail.com>
2026-05-26 17:26:53 +0000

    Added Day6  Python Practice

---
+++ day06.py
@@ -0,0 +1,20 @@
+#While loop
+i=1
+while i<=5:
+    print(i)
+    i+=1
+# Reverse print
+i=5
+while i>=1:
+    print(i)
+    i-=1
+#To print even numbers
+i=2  
+while i<=11:
+    print(i)
+    i+=2
+#To print odd numbers
+i=1
+while i<=10:
+    print(i)
+    i+=2
